3116f48c772c171b179b6c877c85db585d424ae1
Lin Hui <linhui0705@gmail.com>
2020-02-17 22:53:53 +0800

    Singleton

--- src/main/java/net/coderlin/java/demo/pattern/singleton/EnumSingleton.java
+++ src/main/java/net/coderlin/java/demo/pattern/singleton/EnumSingleton.java
@@ -3,6 +3,10 @@ package net.coderlin.java.demo.pattern.singleton;
 /**
  * Title: EnumSingleton
  * Description:
+ * 枚举
+ * 是否Lazy初始化：否
+ * 是否多线程安全：是
+ * 实现难度：易
  *
  * @author Lin Hui
  * Created on 2020/2/17 10:42 下午
--- src/main/java/net/coderlin/java/demo/pattern/singleton/StaticInnerClassSingleton.java
+++ src/main/java/net/coderlin/java/demo/pattern/singleton/StaticInnerClassSingleton.java
@@ -3,6 +3,10 @@ package net.coderlin.java.demo.pattern.singleton;
 /**
  * Title: StaticInnerClassSingleton
  * Description:
+ * 登记式/静态内部类
+ * 是否Lazy初始化：是
+ * 是否多线程安全：是
+ * 实现难度：一般
  *
  * @author Lin Hui
  * Created on 2020/2/17 10:47 下午
